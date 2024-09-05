fbe19ee7bc5de5a30b3f41377668a2395d45987c
Mayron <mayron_doc@outlook.com>
2024-09-05 17:47:00 -0300

    Comentários no código

--- FibonacciSearch/Program.cs
+++ FibonacciSearch/Program.cs
@@ -1,12 +1,17 @@
 ﻿internal class Program {
     static void Main(string[] args) {
+        // Números responsáveis pela sequência de Fibonacci
         int n1 = 0;
         int n2 = 1;
         int n3 = 0;
 
+        // Validação da entrada do usuário
         Console.Write("Informe um número: ");
         int number = 0;
         if (int.TryParse(Console.ReadLine(), out number)) {
+
+            // Laço de repetição até o número informado ser igual ao último
+            // número da soma ou ser maior que o último número da soma
             while(true) {
 
                 if(n1 == number) {
@@ -24,7 +29,9 @@
         else {
             Console.WriteLine("Digite um número!");
         }
-        
+
+        Console.Write("Aperte ENTER para continuar....");
+        Console.ReadLine();
     }
 }
     
